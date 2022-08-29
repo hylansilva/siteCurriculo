7a6074c2a2423266e0584a724d2b8653e4719b53
lanosilva <hylansilva27@gmail.com>
2022-08-28 21:42:06 -0300

    estregando o site para o github

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+function identificacao(){
+
+    const identificar = `<iframe 
+    src="./assets/pages/indentificacao.html" 
+    frameborder="0" 
+    width="100%" 
+    height="500px">
+    </iframe>`
+    
+    document.getElementById("app").innerHTML = identificar
+}
+
+function formacao(){
+    const formar = `<iframe 
+    src="./assets/pages/formacao.html" 
+    frameborder="0" 
+    width="100%" 
+    height="500px"></iframe>`
+    
+    document.getElementById("app").innerHTML = formar
+}
+
+function expProficional(){
+    const eP = `<iframe 
+    src="./assets/pages/experienciaproficional.html" 
+    frameborder="0" 
+    width="100%" 
+    height="500px"></iframe>`
+    
+    document.getElementById("app").innerHTML = eP
+}
+
+function hobbies(){
+    const hobbie = `<iframe 
+    src="./assets/pages/hobbies.html" 
+    frameborder="0" 
+    width="100%" 
+    height="500px"></iframe>`
+    
+    document.getElementById("app").innerHTML = hobbie
+}
